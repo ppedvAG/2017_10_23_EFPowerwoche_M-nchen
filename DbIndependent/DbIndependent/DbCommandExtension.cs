292c494fe26f36d345e0cddfd1b65689f160033c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-23 16:05:05 +0200

    DB Independent

---
+++ DbIndependent/DbIndependent/DbCommandExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Common;
+
+namespace DbIndependent
+{
+    public static class DbCommandExtension
+    {
+        public static void AddParamterWithValues(this DbCommand command, string parameterName, object value)
+        {
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = parameterName;
+            parameter.Value = value;
+            command.Parameters.Add(parameter);
+        }
+    }
+}
---
+++ DbIndependent/DbIndependent/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DbIndependent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shouldUseSql = true;        // aus der Konfiguration oder ähnlichem geladen/gelesen
+
+            string connectionString;
+            DbProviderFactory factory;
+
+            if (shouldUseSql)
+            {
+                connectionString = "Data Source=.;Initial Catalog=NORTHWND;Integrated Security=True";
+                factory = SqlClientFactory.Instance;
+            }
+            else if (true)
+            {
+                connectionString = "any Access/Oracle/IBM DB2/Whatever Connection string";
+                factory = System.Data.Odbc.OdbcFactory.Instance;
+            }
+            else
+            {
+                connectionString = "any other Access/Oracle/IBM DB2/Whatever Connection string";
+                factory = System.Data.OleDb.OleDbFactory.Instance;
+            }
+
+
+            using (var connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    Console.Write("Bitte geben Sie den Suchbegriff ein: ");
+                    var searchTerm = Console.ReadLine();
+
+                    command.CommandText = @"
+Select Vorname = Firstname
+    , Nachname = Lastname 
+    From Employees 
+    Where Firstname Like @query + '%'";
+
+                    command.AddParamterWithValues("@query", searchTerm);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var firstname = reader["Vorname"] as string;
+                            var lastname = reader["Nachname"] as string;
+
+                            Console.WriteLine($"{firstname} {lastname}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
