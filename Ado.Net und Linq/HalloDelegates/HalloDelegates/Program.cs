ceecd7df4dcb261a11f95730b125647ca975a6d1
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-24 14:13:30 +0200

    Hallo Delegats

---
+++ Ado.Net und Linq/HalloDelegates/HalloDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HalloDelegates
+{
+    internal delegate string MyDelegate(int zahl, double wert);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del = new MyDelegate(MeineMethode);
+
+            var result = del.Invoke(5, 8.9);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        private static string MeineMethode(int i, double d)
+        {
+            return (i + d).ToString();
+        }
+    }
+}
