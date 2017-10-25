cf4b47dade5c3ccfca2e45785511d72e4bd9da48
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-25 11:32:11 +0200

    Add DbContext

---
+++ EntityFramework/HalloCodeFirst/HalloCodeFirst/LostStarsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace HalloCodeFirst
+{
+    internal class LostStarsDbContext : DbContext
+    {
+        public LostStarsDbContext() : base("name=LostStarConnectionString")
+        { }
+        public LostStarsDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        { }
+    }
+}
