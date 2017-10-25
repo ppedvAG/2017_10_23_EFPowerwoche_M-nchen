97dfec000fca4747b1c6b1289be53112c35b0bd4
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-25 14:06:12 +0200

    Add Configuration Attributes to Galaxy Table

--- EntityFramework/HalloCodeFirst/HalloCodeFirst/Models/Galaxy.cs
+++ EntityFramework/HalloCodeFirst/HalloCodeFirst/Models/Galaxy.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HalloCodeFirst.Models
 {
     internal class Galaxy
     {
+        [Key]
+        [Column("GalaxyId")]
         public int Id { get; set; }
+
+        //[Key]
+        //[Column("GalaxyId2", Order = 2)]
+        //public int Id2 { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [Column(TypeName = "date")]
         public DateTime DiscoveryDate { get; set; }
+
+        [Column("GalaxyForm")]
         public GalaxyForm Form { get; set; }
     }
 }
