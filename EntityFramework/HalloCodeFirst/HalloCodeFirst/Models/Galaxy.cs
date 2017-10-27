56ca3a263c99a80a225e0950b0af502c3c43008c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-27 11:32:21 +0200

    Inherit Galaxy from CelestialBody

--- EntityFramework/HalloCodeFirst/HalloCodeFirst/Models/Galaxy.cs
+++ EntityFramework/HalloCodeFirst/HalloCodeFirst/Models/Galaxy.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace HalloCodeFirst.Models
 {
-    public class Galaxy
+    public class Galaxy : CelestialBody
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime DiscoveryDate { get; set; }
         public GalaxyForm Form { get; set; }
-        public string Description { get; set; }
 
         public ICollection<Star> Stars { get; } = new HashSet<Star>();
     }
