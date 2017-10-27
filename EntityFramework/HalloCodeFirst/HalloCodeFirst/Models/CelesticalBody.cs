0a00002038335c8711eebf059e042b28598d7f6c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-27 11:33:40 +0200

    make CelestialBody abstract

--- EntityFramework/HalloCodeFirst/HalloCodeFirst/Models/CelesticalBody.cs
+++ EntityFramework/HalloCodeFirst/HalloCodeFirst/Models/CelesticalBody.cs
@@ -2,7 +2,7 @@
 
 namespace HalloCodeFirst.Models
 {
-    public class CelestialBody
+    public abstract class CelestialBody
     {
         public int Id { get; set; }
         public string Name { get; set; }
